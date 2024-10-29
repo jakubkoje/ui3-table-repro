03685ccf61a269f597d78ef8311c14010e5fa834
Jakub <jakub@eduroam-163-74.fiit.stuba.sk>
2024-10-29 13:05:26 +0100

    first commit

---
+++ app.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        <UButton @click="addRow"> Add Row</UButton>
+        <UTable :data="data" />
+        <div>{{ data }}</div>
+    </div>
+</template>
+
+<script setup>
+const data = ref([]);
+
+const addRow = () => {
+    data.value.push({ id: 1, random: Math.random() });
+};
+</script>
+
+<style>
+@import "tailwindcss";
+@import "@nuxt/ui";
+</style>
